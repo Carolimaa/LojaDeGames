44899f1962633b43675923ab4b757d4bf4529c3e
Carolimaa <carolniver12@gmail.com>
2023-12-14 13:18:06 -0300

    project settings

---
+++ src/test/java/br/com/generation/GamePoint/GamePointApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.generation.GamePoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
